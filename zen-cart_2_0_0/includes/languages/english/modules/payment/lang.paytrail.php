c7f6a9756d493627dda52d06f6836b9e7feb72fe
İhsan Yüz <60288896+krbuk@users.noreply.github.com>
2024-08-08 20:00:50 +0300

    summary

---
+++ zen-cart_2_0_0/includes/languages/english/modules/payment/lang.paytrail.php
@@ -0,0 +1,36 @@
+<?php
+$define = [
+	'MODULE_PAYMENT_PAYTRAIL_TEXT_TITLE' => 'Paytrail - Online Payment',
+	'MODULE_PAYMENT_PAYTRAIL_TEXT_DESCRIPTION' => 'Paytrail - ASETUKSET <br>Order Paytrail using the form below. We process orders within one business day. Activating credit card payments requires company information and that the delivery, return and payment terms of the online store are in order
+		   <a href="https://www.paytrail.com" target="_blank">Get more information</a><br><br>
+		   <a href="https://merchant.paytrail.com" target="_blank">Log in to your Paytrail account</a>',
+	'MODULE_PAYMENT_PAYTRAIL_HEADER_ERROR' => 'Error!',
+	'MODULE_PAYMENT_PAYTRAIL_TEXT_ERROR' => 'Error in Paytrail payment!',
+	'MODULE_PAYMENT_PAYTRAIL_ALERT_TEST' => 'Attention: test mode',
+	'MODULE_PAYMENT_PAYTRAIL_ERROR' => 'Payment failed.',
+	'MODULE_PAYMENT_PAYTRAIL_TEXT_API_ERROR' => '<b>ERROR</b> Paytrail online payment -HMAC- signatures incorrect!',
+	'MODULE_PAYMENT_PAYTRAIL_REWARD_POINT_TEXT' => 'Reward points',
+	'MODULE_PAYMENT_PAYTRAIL_COUPON_TEXT' => 'Discount Coupons',
+	'MODULE_PAYMENT_PAYTRAIL_GIFT_TEXT' => 'Gift Cards',
+	'MODULE_PAYMENT_PAYTRAIL_LOWORDER_TEXT' => 'Small Order Extra',
+	'MODULE_PAYMENT_PAYTRAIL_FREE_SHPING' => 'Free delivery',
+	'MODULE_PAYMENT_PAYTRAIL_GROUP_TEXT' => 'Group discount',
+	'MODULE_PAYMENT_PAYTRAIL_SUM_ROUND' => 'Sum round',
+	// Order more information
+	'MODULE_PAYMENT_PAYTRAIL_SELECET_PAYMENT' => 'Choose payment method',
+	'MODULE_PAYMENT_PAYTRAIL_TITLE_STATUS' => 'Paytrail payment status : ', 
+	'MODULE_PAYMENT_PAYTRAIL_PAYMENT_METHOD' => 'Payment method : ',
+	'MODULE_PAYMENT_PAYTRAIL_REFERENCE_NUMBER' => 'Reference Number : ',
+	'MODULE_PAYMENT_PAYTRAIL_PAYMENT_ERROR' => 'ERROR ! Paytrail online payment service does not respond.',
+	'MODULE_PAYMENT_PAYTRAIL_SELECET_OTHER' => 'Kilikka takaisin nappi ja valitse muu maksutapa',
+	// Payment information
+	'MODULE_PAYMENT_PAYTRAIL_PAYMENT_OK' => '[Payment completed] ',
+	'MODULE_PAYMENT_PAYTRAIL_PAYMENT_FAIL' => '[Payment status canceled] ',
+	'MODULE_PAYMENT_PAYTRAIL_PAYMENT_PENDING' => '[Payment is not yet completed] ',
+	'MODULE_PAYMENT_PAYTRAIL_PAYMENT_DELAYED' => '[Payment completed, requires approval on the Paytrail Merchant Panel!] ',
+	'MODULE_PAYMENT_PAYTRAIL_PAYMENT_NEW' => '[Payment has been created but nothing more] ',
+	'MODULE_PAYMENT_PAYTRAIL_ORDER_NUMBER' => 'Order number : ',
+	'MODULE_PAYMENT_PAYTRAIL_PAYMENT_AUTHRORIZED' => 'Payment verified.',
+	'MODULE_PAYMENT_PAYTRAIL_PAYMENT_SETTLED' => 'Payment charged.',
+];
+return $define;
---
+++ zen-cart_2_0_0/includes/languages/finnish/modules/payment/lang.paytrail.php
@@ -0,0 +1,37 @@
+<?php
+$define = [
+	'MODULE_PAYMENT_PAYTRAIL_TEXT_TITLE' => 'Paytrail - Verkkomaksu',
+	'MODULE_PAYMENT_PAYTRAIL_TEXT_DESCRIPTION' => 'Paytrail - ASETUKSET <br>Tilaa Paytrail oheisella lomakkeella. Käsittele tilaukset yhden arkipäivän kuluessa. Luottokorttimaksujen aktivoiminen edellyttää yrityksen tietoja sekä sitä, että verkkokaupan toimitus-, palautus- ja maksuehdot ovat kunnossa
+		   <a href="https://www.paytrail.com" target="_blank">Hanki lisätietoja</a><br><br>
+		   <a href="https://merchant.paytrail.com" target="_blank">Kirjaudu Paytrail-tiliisi</a>',
+	'MODULE_PAYMENT_PAYTRAIL_HEADER_ERROR' => 'Virhe!',
+	'MODULE_PAYMENT_PAYTRAIL_TEXT_ERROR' => 'Virhe Paytrail -maksussa!',
+	'MODULE_PAYMENT_PAYTRAIL_ALERT_TEST' => 'Huomio: Testitila',
+	'MODULE_PAYMENT_PAYTRAIL_ERROR' => 'Maksu epäonnistui.',
+	'MODULE_PAYMENT_PAYTRAIL_TEXT_API_ERROR' => '<b>VIRHE</b> Paytrail verkkomaksu -HMAC-allekirjoituksiin väärin!',
+	'MODULE_PAYMENT_PAYTRAIL_REWARD_POINT_TEXT' => 'Bonuspisteet',
+	'MODULE_PAYMENT_PAYTRAIL_COUPON_TEXT' => 'Alennuskupongit',
+	'MODULE_PAYMENT_PAYTRAIL_GIFT_TEXT' => 'Lahjakortit',
+	'MODULE_PAYMENT_PAYTRAIL_LOWORDER_TEXT' => 'Pientilauslisä',
+	'MODULE_PAYMENT_PAYTRAIL_FREE_SHPING' => 'Ilmainen toimitus',
+	'MODULE_PAYMENT_PAYTRAIL_GROUP_TEXT' => 'Ryhmä alennus',
+	'MODULE_PAYMENT_PAYTRAIL_SUM_ROUND' => 'Summa pyöreä',
+	// Order more information
+	'MODULE_PAYMENT_PAYTRAIL_SELECET_PAYMENT' => 'Valitse maksutapa',
+	'MODULE_PAYMENT_PAYTRAIL_TITLE_STATUS' => 'Paytrail -maksun tila : ', 
+	'MODULE_PAYMENT_PAYTRAIL_PAYMENT_METHOD' => 'Maksutapa : ',
+	'MODULE_PAYMENT_PAYTRAIL_REFERENCE_NUMBER' => 'Viitenumero : ',
+	'MODULE_PAYMENT_PAYTRAIL_PAYMENT_ERROR' => 'Virhe! Paytrail verkkomaksupalvelu ei vastaa. ',
+	'MODULE_PAYMENT_PAYTRAIL_SELECET_OTHER' => 'Kilikka takaisin nappi ja valitse muu maksutapa',
+	// Payment information
+	'MODULE_PAYMENT_PAYTRAIL_PAYMENT_OK' => '[Maksu suoritettu] ',
+	'MODULE_PAYMENT_PAYTRAIL_PAYMENT_FAIL' => '[Maksun tila peruttu] ',
+	'MODULE_PAYMENT_PAYTRAIL_PAYMENT_PENDING' => '[Maksu ei vielä suoritettu] ',
+	'MODULE_PAYMENT_PAYTRAIL_PAYMENT_DELAYED' => '[Maksu suoritettu, vaatii hyväksynnän Paytrail kauppiaspaneeli!] ',
+	'MODULE_PAYMENT_PAYTRAIL_PAYMENT_NEW' => '[Maksu on luotu, mutta ei mitään muuta] ',
+	'MODULE_PAYMENT_PAYTRAIL_ORDER_NUMBER' => 'Tilausnumero : ',
+	'MODULE_PAYMENT_PAYTRAIL_PAYMENT_AUTHRORIZED' => 'Maksu varmennettu.',
+	'MODULE_PAYMENT_PAYTRAIL_PAYMENT_SETTLED' => 'Maksu veloitettu.',
+];
+
+return $define;
